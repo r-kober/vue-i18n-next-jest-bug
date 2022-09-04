51fab995e98cfd959ed57bdc4cf4197fc0e37fa8
Roman Kober <ray2110@gmail.com>
2022-09-04 23:30:31 +0200

    implemented small project to show the jest problem with the global locale

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	preset: "@vue/cli-plugin-unit-jest",
+	testMatch: ["**/__tests__/*.{j,t}s?(x)", "**/*.spec.{j,t}s?(x)"],
+};
---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,34 @@
+<template>
+	<img
+		src="https://flagcdn.com/de.svg"
+		width="24"
+		title="Deutsch"
+		alt="German"
+		class="me-1"
+		@click="selectLanguage('de')" />
+	<img
+		src="https://flagcdn.com/gb.svg"
+		width="24"
+		alt="English"
+		title="English"
+		@click="selectLanguage('en')" />
+</template>
+
+<script>
+export default {
+	name: "LanguageSelector",
+	methods: {
+		selectLanguage(language) {
+			console.log(`change language to ${language}`);
+			this.$i18n.locale = language;
+			console.log(`i18n.locale = ${this.$i18n.locale}`);
+		},
+	},
+};
+</script>
+
+<style scoped>
+img {
+	cursor: pointer;
+}
+</style>
